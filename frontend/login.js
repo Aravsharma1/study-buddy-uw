4d8e4552c9559c88136be2ad2d6954d4ccb6cc79
Aravsharma1 <aravsharma0905@gmail.com>
2024-10-01 23:22:51 -0400

    Initial Commit

---
+++ frontend/login.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const path = require('path');
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+
+dotenv.config({path: './.env'});
+
+const app = express();
+app.get("/", (req, res) => {
+    //res.send("<h1> Login form page") // res is used to send data to the frontend of an application
+    res.render("index");
+});
+
+app.get("/register", (req, res) => {
+    //res.send("<h1> Login form page") // res is used to send data to the frontend of an application
+    res.render("register");
+});
+
+/*
+importing mysql
+*/
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST, // server, since its running locally so local host
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE   
+});
+
+const publicDirectory = path.join(__dirname, './public') // this directory includes all css, js files
+app.use(express.static(publicDirectory));
+
+app.set("view engine", 'hbs'); // setting the html
+
+db.connect( (error) => {
+    if(error) {
+        console.log(error)
+    }else{
+        console.log("MYSQL Connected")
+    }
+})
+
+app.listen(5001, () => {
+    console.log("Server started on Port 5001")
+});
---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Dashboard from './components/Dashboard';
+import LoginForm from '../login';
+
+const App = () => {
+    return (
+        <div className="App">
+            
+            <LoginForm />
+        </div>
+    );
+};
+
+export default App;
